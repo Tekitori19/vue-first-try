69436b4637ff9c7372880d0f88902e2d3a44528a
ginna <corclan19@gmail.com>
2023-10-29 01:58:56 +0700

    syntax

---
+++ syntax/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Hello world</h1>
+  <h2>user: {{ user }}</h2>
+  <hr />
+  <Races />
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import RacesPony from '@/components/Races.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Races: RacesPony
+  },
+  setup() {
+    const user = ref({
+      id: 1,
+      name: 'Dinh'
+    })
+
+    return { user }
+  }
+})
+</script>
---
+++ syntax/src/components/Races.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>Races</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'RacesPony'
+})
+</script>
